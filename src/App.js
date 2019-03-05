2b65860dd03e28e7bd0465caab10de8af8e9bf26
rebecalvarez <rebeca.dodero@gmail.com>
2019-03-04 21:28:12 -0600

    updating styling

--- src/App.js
+++ src/App.js
@@ -24,22 +24,32 @@ class App extends Component {
     this.setState({cards:shuffleArray});
     //if clicked an image already clicked set this.state.score = 0
     if (this.state.clickedArray.includes(id)){
-      this.setState({score: 0, clickedArray:[], message: "Sorry, start game again!", shakeit: "true"});
-    } 
+      this.setState({score: 0, clickedArray:[], message: "Sorry, start the game again!", shakeit: "true"});
+    }  
     else {
       this.setState({
         clickedArray: this.state.clickedArray.concat([id]),
         score: this.state.score + 1,
-        message: "Correct",
+      message: "Correct!",
         shakeit: "false"
       });
     }
+
+    if(this.state.score === 14 ) {
+      this.setState({
+        topScore: this.state.score,
+        clickedArray:[],
+        message: "Congratulations, You Won the game! You can keep playing and adding to the top score",
+        shakeit: "true"
+      });}
     // set topscore = score if score > topscore.
     if(this.state.score > this.state.topScore) {
       this.setState({topScore: this.state.score});
     }
     // shake the wrapper if shakeit it set to true
+    
   }
+
   shuffleArray = (picturesArray)=> {
     for (let i = picturesArray.length - 1; i > 0; i-- ){
       const j = Math.floor(Math.random() * (i + 1));
@@ -51,15 +61,20 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      <header className="App-header">
-     <img src={logo} className = "App-logo" alt="logo"/>
-     <h1 className="App-title">Welcome to React Clicky Game</h1>
+      <header className="App-header  sticky-top">
+      <div className="text-center"><img src={logo} className = "App-logo" alt="logo"/>
+     <h2 className="App-title">Welcome to React Clicky Game</h2>
+     <p className="score"><strong>Score: {this.state.score} | TopScore: {this.state.topScore}</strong></p>
+     </div>
+     
+     
       </header>
+      <div className="banner">
       <h3 className="App-intro">
-      <strong>Click on the image to earn points, but don't click it more than once!</strong>
-      <p className="score"><strong>Score: {this.state.score} | TopScore: {this.state.topScore}</strong></p>
-      <p className="message"><strong> {this.state.message}</strong></p>
+      <strong className="instructions">Click on the image to earn points, but don't click it more than once!</strong>
       </h3>
+      <h2 className="message"><strong> {this.state.message}</strong></h2>
+      </div>
   <Wrapper
       shakeWrapper = {this.state.shakeit}
       pictures = {this.state.cards.map(picture => (
